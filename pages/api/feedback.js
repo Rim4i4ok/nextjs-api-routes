4e4c0dffec2dc1c3f1c8393f92523d9dc3aba08b
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-29 23:53:15 +0300

    feedback api

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({ message: "Hello world" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    // store in some place
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "Hello world" });
+  }
 }
 
 export default handler;
