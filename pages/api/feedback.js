fd86bfc71e4d9160ad798e75cf2153b15a8a11bb
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-30 00:02:55 +0300

    sending feedback from ui

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -4,7 +4,7 @@ import path from "path";
 function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
-    const feedbackText = req.body.feedback;
+    const feedbackText = req.body.text;
 
     const newFeedback = {
       id: new Date().toISOString(),
@@ -15,8 +15,10 @@ function handler(req, res) {
     // store in some place
     const filePath = path.join(process.cwd(), "data", "feedback.json");
     const fileData = fs.readFileSync(filePath);
+
     const data = JSON.parse(fileData);
     data.push(newFeedback);
+
     fs.writeFileSync(filePath, JSON.stringify(data));
 
     res.status(201).json({ message: "Success!", feedback: newFeedback });
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRed = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const email = emailInputRed.current.value;
+    const feedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: email, text: feedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resonse) => resonse.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type={"email"} id="email" ref={emailInputRed} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feeddback" rows={3} ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Submit feedback</button>
+      </form>
     </div>
   );
 }
